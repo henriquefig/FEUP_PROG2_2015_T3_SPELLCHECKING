5141f8bd4deeb7cf0841fc1796df67e60cae8782
Henrique Figueiredo <nf.henrique@gmail.com>
2017-05-03 09:44:03 +0100

    Add files via upload

--- spellChecker.c
+++ spellChecker.c
@@ -1,16 +1,110 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "tabdispersao.h"
 
 // #include & #define
 // functions and procedures
+long int erros=0, sugestoes=0;
+void str_tolower(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        str[i] = tolower(str[i]);
+        i++;
+    }
+}
+int checktable_disp(tabela_dispersao *t,char *str)
+{
+	char wsugestion[15][30];
+	char aux[30];
+	int count=0;
+	int wnsugest=0;
+	str_tolower(str);
+	printf("%s\n",str);
+	for(int i=0;i<strlen(str);i++)
+	{
+		while(str[i]=='(' || str[i]=='"' || str[i]==')' || str[i]==',')
+			i++;
+		if(str[i]=='-' || str[i]=='\n' || isdigit(str[i]))
+			str[i]=' ';
+		if(str[i]==' ' || i==strlen(str)-1)
+		{
+			aux[count]='\0';
+			if(i==strlen(str)-1)
+				aux[count]=str[i];
+			count=0;
+			//printf("Palavra - %s\n",aux);
+			if(tabela_valor(t,aux)!=NULL)
+			{
+			    memset(wsugestion,0,sizeof(wsugestion));
+			    wnsugest=0;
+			}
+			else
+			{
+				printf("\tWord not found: %s\n",aux);
+				printf("\tPerhaps you meant: ");
+				for(int h=0;h<wnsugest;h++)
+					if(h!=wnsugest-1)
+			     	printf("%s, ",wsugestion[h]);
+			     printf("%s\n",wsugestion[wnsugest-1]);
+			     memset(wsugestion,0,sizeof(wsugestion));
+			     sugestoes=sugestoes+wnsugest;
+			     wnsugest=0;
+			        		
+				 erros++;
+			}
+		}
+		else
+		{
+			aux[count]=str[i];
+			count++;
+		}
+	}
+}
+int readworlist(tabela_dispersao *twords,FILE * finput)
+{
+	char *str=calloc(300,1);
+	char c;
+	int count=0;
+	while(fscanf(finput,"%c",&c)!=EOF)
+    {
 
+    	//Se chegar ao final da linha
+    	if(c=='.' || c=='!' || c=='?' || c==';')
+    	{
+    		if(strstr(str,"Online-Utility")!=NULL)
+    		{
+    			char rest[40];
+    			fgets(rest,20,finput);
+    			strcat(str,rest);
+    			fgets(rest,20,finput);
+    			strcat(str,rest);
+    			fgets(rest,20,finput);
+    			strcat(str,rest);
+    		}
+    		else
+	    		str[count]='\0';
+    		count=0;
+    		if(strstr(str,"Online-Utility")==NULL)
+			{
+				checktable_disp(twords,str);
+			}
+			str=calloc(300,1);
+    	}
+    	else
+    	{
+	    	//Guarda o caracter na String
+	    	str[count]=c;
+	    	count++;	
+    	}
+    }
+}
 int main(int argc, char* argv[])
 {
 	FILE *input, *wordlist;
 	clock_t inicio, fim;
-  double tempo;
-	long int erros, sugestoes;
+  double tempo=0;
 
 	if(argc != 3)
 	{
@@ -38,6 +132,8 @@ int main(int argc, char* argv[])
 
 	if(!strcmp(argv[2],"verboso")){
 		inicio = clock();
+		tabela_dispersao *t=readfiletable(wordlist);
+		readworlist(t,input);
 		// processa e imprime output como indicado no enunciado
 		// guarda na variável erros o número de palavras com ortografia errada encontradas
 		// guarda na variável sugestoes o número de palavras sugeridas para corrigir palavras com ortografia errada encontradas
