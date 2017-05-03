f2bae99cc0a2d7f7017bca0b09ca19bd60ace31b
Henrique Figueiredo <nf.henrique@gmail.com>
2017-05-03 09:46:12 +0100

    Add files via upload

--- spellChecker.c
+++ spellChecker.c
@@ -2,11 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "tabdispersao.h"
 
 // #include & #define
 // functions and procedures
 long int erros=0, sugestoes=0;
+int trocados(char * src,char *dst)
+{
+	if(strlen(src)==strlen(dst))
+	{
+		for(int i=0;i<strlen(src);i++)
+		{
+			for(int j=strlen(dst);j>0;j--)
+			{
+
+				if(i!=j && src[i]==dst[j] && src[j]==dst[i])
+				{
+					char aux[100];
+					for(int h=0;h<strlen(src);h++)
+					{
+						aux[h]=src[h];
+						if(h==i)
+							aux[h]=src[j];
+						if(h==j)
+							aux[h]=src[i];
+					}
+					if(strcmp(aux,dst)==0)
+						return 0;
+				}
+			}
+		}
+	}
+	return 1;
+}
 void str_tolower(char *str) {
     int i = 0;
     while (str[i] != '\0') {
@@ -14,55 +41,115 @@ void str_tolower(char *str) {
         i++;
     }
 }
-int checktable_disp(tabela_dispersao *t,char *str)
+
+int checkditionary(FILE * fword,char *word)
 {
+	char str;
+	char *aux=calloc(50,1);
+	char *newword=calloc(100,1);
 	char wsugestion[15][30];
-	char aux[30];
-	int count=0;
 	int wnsugest=0;
-	str_tolower(str);
-	printf("%s\n",str);
-	for(int i=0;i<strlen(str);i++)
+	int count=0,count2=0,flag=0;
+	int once=0;
+	for(int i=0;i<strlen(word);i++)
 	{
-		while(str[i]=='(' || str[i]=='"' || str[i]==')' || str[i]==',')
+		while(word[i]=='(' || word[i]=='"' || word[i]==')' || word[i]==',')
 			i++;
-		if(str[i]=='-' || str[i]=='\n' || isdigit(str[i]))
-			str[i]=' ';
-		if(str[i]==' ' || i==strlen(str)-1)
+		if(word[i]=='-' || word[i]=='\n' || isdigit(word[i]))
+			word[i]=' ';
+		if(word[i]==' ' || i==strlen(word)-1)
 		{
-			aux[count]='\0';
-			if(i==strlen(str)-1)
-				aux[count]=str[i];
-			count=0;
-			//printf("Palavra - %s\n",aux);
-			if(tabela_valor(t,aux)!=NULL)
+			if(count!=0)
 			{
-			    memset(wsugestion,0,sizeof(wsugestion));
-			    wnsugest=0;
-			}
-			else
-			{
-				printf("\tWord not found: %s\n",aux);
-				printf("\tPerhaps you meant: ");
-				for(int h=0;h<wnsugest;h++)
-					if(h!=wnsugest-1)
-			     	printf("%s, ",wsugestion[h]);
-			     printf("%s\n",wsugestion[wnsugest-1]);
-			     memset(wsugestion,0,sizeof(wsugestion));
-			     sugestoes=sugestoes+wnsugest;
-			     wnsugest=0;
-			        		
-				 erros++;
+				aux[count]='\0';
+				if(i==strlen(word)-1)
+					aux[count]=word[i];
+				count=0;
+				while(fscanf(fword,"%c",&str)!=EOF)
+			    {
+
+			    	//Se chegar ao final da linha
+			    	if(str=='\n')
+			    	{
+			    		newword[count2]='\0';
+				        //Faz reset ao contador de caracteres e à linha
+				        count2=0;
+				        str_tolower(aux);
+				        str_tolower(newword);
+				        if(strcmp(aux,newword)==0)
+				        {
+				        	flag=0;
+						    memset(wsugestion,0,sizeof(wsugestion));
+						    wnsugest=0;
+				        	break;
+				        }
+				        else
+				       	{
+				       		if(trocados(aux,newword)==0)
+				        	{
+				        		int superflag=0;
+				        		if(wnsugest==0)
+				        		{
+			        				strcat(wsugestion[wnsugest],newword);
+			        				wnsugest++;
+				        		}
+						        for(int h=0;h<wnsugest;h++)
+						        {
+						        	if(strcmp(newword,wsugestion[h])==0)
+				        			{
+				        				superflag=1;
+				        			}
+				        		}
+				        		if(superflag!=1)
+				        		{
+			        				strcat(wsugestion[wnsugest],newword);
+			        				wnsugest++;
+				        		}
+				        	}
+				        	flag=1;
+				       	}
+			        	//Liberta e realoca memoria
+			        	newword=calloc(100,1);
+			    	}
+			    	else
+			    	{
+				    	//Guarda o caracter na String
+				    	newword[count2]=str;
+				    	count2++;	
+			    	}
+			    }
+			    if(flag==1)
+			    {
+			    	if(once==0)
+			    	{
+						printf("%s\n",word);
+						once=1;
+					}
+					printf("\tWord not found: %s\n",aux);
+					printf("\tPerhaps you meant: ");
+					for(int h=0;h<wnsugest;h++)
+						if(h!=wnsugest-1)
+				     	printf("%s, ",wsugestion[h]);
+				     printf("%s\n",wsugestion[wnsugest-1]);
+				     memset(wsugestion,0,sizeof(wsugestion));
+				     sugestoes=sugestoes+wnsugest;
+				     wnsugest=0;
+				        		
+					 erros++;
+			    }
+			    rewind(fword);
+				aux=calloc(50,1);
 			}
 		}
 		else
 		{
-			aux[count]=str[i];
+			aux[count]=word[i];
 			count++;
 		}
 	}
+	
 }
-int readworlist(tabela_dispersao *twords,FILE * finput)
+int readworlist(FILE *fword,FILE * finput)
 {
 	char *str=calloc(300,1);
 	char c;
@@ -88,7 +175,7 @@ int readworlist(tabela_dispersao *twords,FILE * finput)
     		count=0;
     		if(strstr(str,"Online-Utility")==NULL)
 			{
-				checktable_disp(twords,str);
+				checkditionary(fword,str);
 			}
 			str=calloc(300,1);
     	}
@@ -132,8 +219,7 @@ int main(int argc, char* argv[])
 
 	if(!strcmp(argv[2],"verboso")){
 		inicio = clock();
-		tabela_dispersao *t=readfiletable(wordlist);
-		readworlist(t,input);
+		readworlist(wordlist,input);
 		// processa e imprime output como indicado no enunciado
 		// guarda na variável erros o número de palavras com ortografia errada encontradas
 		// guarda na variável sugestoes o número de palavras sugeridas para corrigir palavras com ortografia errada encontradas
